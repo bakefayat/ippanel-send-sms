fd1d1dfcbdce98dad7316c98ca90204ed94d4bb7
bakefayat <bakefayat98@gmail.com>
2022-02-16 10:53:50 +0330

    env-sample added to project.

--- send_pattern_with_url_from_excel.py
+++ send_pattern_with_url_from_excel.py
@@ -1,7 +1,9 @@
 import requests
 import read_from_file
 from config import api_key, pid, sender
-# read from excel file and load the set with names and price.
+
+
+# read from Excel file and load the set with names and price.
 user_set = read_from_file.add_to_set()
 
 # first key.
@@ -10,10 +12,12 @@ p1 = 'name'
 p2 = 'price'
 
 # value of first and second key is different for every user.
-for i in user_set:
-    v1 = i[0]
-    v2 = i[1]
-    tnum = i[2]
-    url = f'http://ippanel.com:8080/?apikey={api_key}&pid={pid}&fnum={sender}&tnum={tnum}&p1={p1}&p2={p2}&v1={v1}&v2={v2}'
+for user in user_set:
+    # first value.
+    v1 = user[0]
+    # second value.
+    v2 = user[1]
+    receiver = user[2]
+    url = f'http://ippanel.com:8080/?apikey={api_key}&pid={pid}&fnum={sender}&tnum={receiver}&p1={p1}&p2={p2}&v1={v1}&v2={v2}'
     a = requests.get(url)
     print(a)
