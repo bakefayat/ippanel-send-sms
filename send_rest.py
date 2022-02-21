ff34026ff70414869af6ab4d9c3ea2d4d07a752e
bakefayat <bakefayat98@gmail.com>
2022-02-21 16:47:09 +0330

    send sms with REST to multiple receivers.

---
+++ send_rest.py
@@ -0,0 +1,24 @@
+import requests
+import json
+from config import api_key
+url_address = "http://rest.ippanel.com/v1/messages/"
+sender = "+983000505"
+receiver = ["+989999999999", ]
+message_text = "متن تست"
+
+bytes_response = requests.post(url=url_address,
+                               headers={"Authorization": f"AccessKey {api_key}"},
+                               data={"originator": sender,
+                                     "recipients": receiver,
+                                     "message": message_text})
+response = json.loads(bytes_response.content)
+try:
+    bulk_id = response['data']['bulk_id']
+    print(f"message sent. bulk id is: {bulk_id}")
+
+except KeyError:
+    message = {
+        "status": response['status'],
+        "error": response['data']['error']
+    }
+    print(message['status'], message['error'])
