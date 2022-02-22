7d3d260c04e8eee94b24b835a130e4b83b5673cd
bakefayat <bakefayat98@gmail.com>
2022-02-22 18:10:16 +0330

    credit_rest now is easy to use and endpoint now defined on core.py

--- core.py
+++ core.py
@@ -1,24 +1,41 @@
 import requests
 import json
+from config import api_key, base_endpoint
+
+
+def response_content_to_json(bytes_response):
+    json_response = json.loads(bytes_response.content)
+    return json_response
 
 
 class RestUrl:
-    def __init__(self, address, api_key):
-        self.address = address
-        self.api_key = api_key
-        self.response = None
+    def get_credit(self):
+        address = base_endpoint + 'v1/credit'
+        try:
+            bytes_response = requests.get(url=address,
+                                          headers={"Authorization": f"AccessKey {api_key}"})
+            json_response = response_content_to_json(bytes_response)
+            response = json_response['data']['credit']
+            return response
 
-    def get_request_to_url(self):
-        self.response = requests.get(url=self.address,
-                                     headers={"Authorization": f"AccessKey {self.api_key}"})
-        return self.response
+        except KeyError:
+            res = {
+                "status": json_response['status'],
+                "error": json_response['data']['error']
+            }
+            return res
+
+        except requests.exceptions.ConnectionError:
+            response = "connection refused."
+
+    def get_user_detail(self):
+        address = self.endpoint + 'v1/user'
+        response = requests.get(url=address,
+                                headers={"Authorization": f"AccessKey {api_key}"})
+        return response
 
     def post_request_to_url(self, data):
-        self.response = requests.post(url=self.address,
-                                      headers={"Authorization": f"AccessKey {self.api_key}"},
-                                      data=data)
-        return self.response
-
-    def response_content_to_json(self):
-        message = json.loads(self.response.content)
-        return message
+        response = requests.post(url=self.address,
+                                 headers={"Authorization": f"AccessKey {api_key}"},
+                                 data=data)
+        return response
--- credit_rest.py
+++ credit_rest.py
@@ -1,17 +1,7 @@
-from config import api_key
+import requests.exceptions
+
 from core import RestUrl
 
-url = RestUrl('http://rest.ippanel.com/v1/credit', api_key)
-url.get_request_to_url()
-response = url.response_content_to_json()
-
-try:
-    credit = int(response['data']['credit'])
-    print(f"your credit is: {credit} rials.")
-
-except KeyError:
-    message = {
-        "status": response['status'],
-        "error": response['data']['error']
-    }
-    print(message['status'], message['error'])
+url = RestUrl()
+response = url.get_credit()
+print(response)
